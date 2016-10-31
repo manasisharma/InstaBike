813d368cb92dcc6bbfa27384c50ffc10751ce314
Manasi Sharma <linkin2manasi@gmail.com>
2016-10-31 10:46:02 -0400

    Adding human computer interaction solution for bike rental

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # InstaBike
+
+1. open the consoleapplication4
+2. run program.cs
+(It will show the server side of the code)
+3. open windowsformapplication5
+4. run windowsformsaaplication5
+(this is the client side of the code)
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+            }
+         
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show("Application has encountered error....");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readData = "Conected to Chat Server ...";
+            msg();
+            clientSocket.Connect("127.0.0.0", 8888);
+            label1.Text = "Client Socket Program - Server Connected ";
+            serverStream = clientSocket.GetStream();
+            string getdata ="https://www.googleapis.com/language/translate/v2?key=AIzaSyCkjqAr5DZveo622PJGikqtVlf6FL8T7Qo&source=en&target=de&callback=translateText&q=";
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(getdata + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            Thread ctThread = new Thread(getMessage);
+            ctThread.Start();
+        }
+        private void getMessage()
+        {
+            while (true)
+            {
+                serverStream = clientSocket.GetStream();
+                int buffSize = 0;
+                byte[] inStream = new byte[10025];
+                buffSize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffSize);
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = "" + returndata;
+                msg();
+            }
+        }
+        private void msg()
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(msg));
+            else
+                textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + readData;
+        } 
+
+    }
+}
